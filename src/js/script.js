dd2af8ace4f4296077647ce1f0c177d5db35bdd4
Marcin Skowronek <marcinfoto35@op.pl>
2021-05-24 11:46:08 +0200

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,28 @@
+{
+  'use strict';
+
+  const select = {
+    templateOf: {
+      bookCart: '#template-book',
+    },
+    containerOf: {
+      book: '#books-list',
+    }
+  }
+
+  const templates = {
+    book: Handlebars.compile(document.querySelector(select.templateOf.bookCart).innerHTML),
+    // booksList: Handlebars.compile(document.querySelector(select.containerOf.book).innerHTML),
+  };
+
+  function render(){
+    const thisBook = this;
+    for(let book of dataSource.books){
+      const generatedHTML = templates.book;
+      thisBook.element = utils.createDOMFromHTML(generatedHTML);
+      const bookContainer = document.querySelector(select.containerOf.book);
+      bookContainer.appendChild(thisBook.element);
+    }
+  }
+  render();
+}
